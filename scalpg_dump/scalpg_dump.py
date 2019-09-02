b12ed897cda1122c78603d0abf7451f949d6690c
Tobias Englert <te@englert.it>
2019-09-02 19:02:00 +0200

    Reformat code

--- scalpg_dump/scalpg_dump.py
+++ scalpg_dump/scalpg_dump.py
@@ -12,17 +12,15 @@ def main():
     args = parser.parse_args()
 
     # Get env vars
-    p1 = subprocess.Popen(['scalingo', 'run', 'env'], stdout=subprocess.PIPE)
+    p1 = subprocess.Popen(["scalingo", "run", "env"], stdout=subprocess.PIPE)
     p2 = subprocess.Popen(
-        ['grep', 'postgres://'],
-        stdin=p1.stdout,
-        stdout=subprocess.PIPE
+        ["grep", "postgres://"], stdin=p1.stdout, stdout=subprocess.PIPE
     )
     p1.stdout.close()
     output, err = p2.communicate()
 
     # Get db_url
-    db_url = output.decode("utf-8").split('=')[1].split('\r\n')[0]
+    db_url = output.decode("utf-8").split("=")[1].split("\r\n")[0]
 
     # Parse db_url
     db_config = dj_database_url.parse(db_url)
@@ -34,7 +32,7 @@ def main():
     command += "pg_dump --clean --host {HOST} --port {PORT} "
     command += "--username {USER} "
     command += "--no-owner --no-privileges "
-    command += "--exclude-schema \'information_schema\' "
+    command += "--exclude-schema 'information_schema' "
     command += "--exclude-schema '^pg_*' --dbname {NAME}"
     command = '"' + command + '"'
     command = command.format(**db_config)
